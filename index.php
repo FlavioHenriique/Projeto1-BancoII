af1c7d86997485820affb4aa4478898e6e5bf1ed
Flavio <flavio_henrique457@hotmail.com>
2017-12-22 16:29:30 -0200

    Commit inicial

---
+++ index.php
@@ -0,0 +1,12 @@
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>teste</title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
