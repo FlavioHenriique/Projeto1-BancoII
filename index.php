c129167830c1158e0d0f93d1c4021495aa668706
Flavio <flavio_henrique457@hotmail.com>
2017-12-23 09:10:56 -0200

    Organizando página index em uma tabela

--- index.php
+++ index.php
@@ -3,29 +3,47 @@
     <head>
         <meta charset="UTF-8">
         <title>Projeto 1</title>
-        <link rel="stylesheet" href="mapa.css">
+        <link rel="stylesheet" href="app.css">
     </head>
     <body>
-
+        <table width="100%" border=1 height="80%"><tr>
+                <td width="50%"> <h1>Tela Inicial</h1></td>
+                <td>
+                    <div id="login"><form method="post" tex-align="right">
+                        <table id="login">
+                            <tr>
+                                <td> Email</td>     
+                                <td>Senha</td>
+                            </tr>
+                            <tr>
+                                <td><input type="text" name="email"></td>
+                                <td><input type="password" name="senha"></td>
+                                <td> <input type="submit" value="Entrar"></td>
+                            </tr>
+                            <tr><td><label id="autenticacao"></label></td></tr>
+                        </table>           
+                        </form></div>
+                </td>
+            </tr>
+            <tr height="90%">
+                <td>  <div id="map"></div></td>
+                <td>
+                    <form><center>
+                            <h2>Cadastro de Usuário</h2>
+                            Nome <input type="text" name="cadNome"> <br><br>
+                            Email <input type="text" name="cadEmail"><br><br>
+                            Senha <input type="password" name="cadSenha"><br><br>
+                            <input type="submit" value="Cadastrar">
+                        </center>
+                    </form>
+                </td>
+            </tr>
+        </table>
         <br>
-        <h1>Tela Inicial</h1>
-        <form method="post" >
-            <table id="login">
-                <tr>
-                    <td> Email</td>     
-                    <td>Senha</td>
-                </tr>
-                <tr>
-                    <td><input type="text" name="email"></td>
-                    <td><input type="password" name="senha"></td>
-                    <td> <input type="submit"></td>
-                </tr>
-                <tr><td><label id="autenticacao"></label></td></tr>
-            </table>
-           
-        </form>
+
+
         <br><br>
-        <div id="map"></div>
+
 
 
     </body>
@@ -34,19 +52,25 @@
 $con = pg_connect("host=localhost port=5432 user=postgres password='flavio22'"
         . "dbname=projetoI-BancoII") or die("erro na conexão com banco!");
 
-if (isset($_POST["email"]) && isset($_POST["senha"])){
+if (isset($_POST["email"]) && isset($_POST["senha"])) {
     $email = $_POST["email"];
     $senha = $_POST["senha"];
     $sql = "SELECT * FROM USUARIO WHERE email='$email' AND senha='$senha'";
-    $result = pg_query($con,$sql);
+    $result = pg_query($con, $sql);
     $row = pg_fetch_array($result);
-    if($row>0){
-        echo "<script>alert('Bem vindo, ".$row["nome"]."!');</script>";
-    }
-    else echo"<script>autenticacao.innerHTML='Usuário não encontrado';</script>"
+    if ($row > 0) {
+        echo "<script>alert('Bem vindo, " . $row["nome"] . "!');</script>";
+    } else {
+        echo"<script>autenticacao.innerHTML='Usuário não encontrado';</script>"
         . "<style>#autenticacao{color:red;}</style>";
-    
+    }
 }
+
+    if (isset($_POST["cadEmail"]) && isset($_POST["cadNome"]) && 
+            isset($_POST["cadSenha"])){
+            
+        
+            }
 
 echo " <script>
       function initMap() {
@@ -56,7 +80,6 @@ echo " <script>
         });
         var infoWindow = new google.maps.InfoWindow({map: map});
 
-        // Try HTML5 geolocation.
         if (navigator.geolocation) {
           navigator.geolocation.getCurrentPosition(function(position) {
             var pos = {
@@ -71,8 +94,7 @@ echo " <script>
             handleLocationError(true, infoWindow, map.getCenter());
           });
         } else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
+         handleLocationError(false, infoWindow, map.getCenter());
         }
       }
 
@@ -85,4 +107,4 @@ echo " <script>
     </script>
     <script src='https://maps.googleapis.com/maps/api/js?key=AIzaSyCHoafMtF7Sv5XiUhhTpnqv82PaGuFM3u4&callback=initMap'
     async defer></script>";
-?>
+?>  
