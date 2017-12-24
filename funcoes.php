53fca9ab744ac0240b8662d4dc9b3dd78cb75189
Flavio <flavio_henrique457@hotmail.com>
2017-12-24 21:11:22 -0200

    Adicionando arquivo controleUsuario.php para as funções de controle de usuário

---
+++ funcoes.php
@@ -0,0 +1,74 @@
+<?php
+
+function getConnection() {
+     $con = pg_connect("host=localhost port=5432 user=postgres password='flavio22'"
+            . "dbname=projetoI-BancoII") or die("erro na conexão com banco!");
+     return $con;
+}
+
+function autenticar($email, $senha) {
+
+    $con = getConnection();
+    $sql = "SELECT * FROM USUARIO WHERE email='$email' AND senha='$senha'";
+    $result = pg_query($con, $sql);
+    $row = pg_fetch_array($result);
+    if ($row > 0) {
+        echo "<script>alert('Bem vindo, " . $row["nome"] . "!');</script>";
+    } else {
+        echo"<script>autenticacao.innerHTML='Usuário não encontrado';</script>"
+        . "<style>#autenticacao{color:red;}</style>";
+    }
+}
+
+function cadastrarUsuario($cadEmail, $cadNome, $cadSenha) {
+
+    $con = getConnection();
+    $sql = "SELECT email FROM Usuario WHERE email='" . $cadEmail . "'";
+    $result = pg_query($con, $sql);
+    if (pg_num_rows($result) > 0) {
+        return false;
+    } else {
+        $cadastro = "INSERT INTO Usuario (nome,email,senha) VALUES('"
+                . $cadNome . "','" . $cadEmail . "','" . $cadSenha . "')";
+        pg_exec($con, $cadastro);
+        return true;
+    }
+}
+
+function initMap() {
+    echo " <script>
+      function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 14
+        });
+        var infoWindow = new google.maps.InfoWindow({map: map});
+
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Você está aqui.');
+            map.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+         handleLocationError(false, infoWindow, map.getCenter());
+        }
+      }
+
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+      }
+    </script>
+    <script src='https://maps.googleapis.com/maps/api/js?key=AIzaSyCHoafMtF7Sv5XiUhhTpnqv82PaGuFM3u4&callback=initMap'
+    async defer></script>";
+}
--- index.php
+++ index.php
@@ -6,13 +6,13 @@
         <link rel="stylesheet" href="app.css">
     </head>
     <body>
-        <table width="100%" border=1 height="80%"><tr>
+        <table width="100%" height="80%" class="tabela"><tr bgcolor="#B0E0E6">
                 <td width="50%"> <h1>Tela Inicial</h1></td>
                 <td>
                     <div id="login"><form method="post" tex-align="right">
-                            <table id="login">
+                            <table>
                                 <tr>
-                                    <td> Email</td>     
+                                    <td>Email</td>     
                                     <td>Senha</td>
                                 </tr>
                                 <tr>
@@ -45,73 +45,28 @@
     </body>
 </html>
 <?php
-$con = pg_connect("host=localhost port=5432 user=postgres password='flavio22'"
-        . "dbname=projetoI-BancoII") or die("erro na conexão com banco!");
+require_once 'funcoes.php';
+
 
 if (isset($_POST["email"]) && isset($_POST["senha"])) {
     $email = $_POST["email"];
     $senha = $_POST["senha"];
-    $sql = "SELECT * FROM USUARIO WHERE email='$email' AND senha='$senha'";
-    $result = pg_query($con, $sql);
-    $row = pg_fetch_array($result);
-    if ($row > 0) {
-        echo "<script>alert('Bem vindo, " . $row["nome"] . "!');</script>";
-    } else {
-        echo"<script>autenticacao.innerHTML='Usuário não encontrado';</script>"
-        . "<style>#autenticacao{color:red;}</style>";
-    }
+    autenticar($email, $senha);
 }
 
 if (isset($_POST["cadEmail"]) && isset($_POST["cadNome"]) &&
         isset($_POST["cadSenha"])) {
 
-    $sql = "SELECT email FROM Usuario WHERE email='" . $_POST['cadEmail'] . "'";
-    $result = pg_query($con, $sql);
-    if (pg_num_rows($result) > 0) {
-        echo "<script> cadastro.innerHTML='Este email já está sendo"
-        . " utilizado!';</script> <style>#cadastro{color:red;}</style>";
-    } else {
-        $cadastro = "INSERT INTO Usuario (nome,email,senha) VALUES('"
-                .$_POST["cadNome"]."','".$_POST["cadEmail"]."','".$_POST["cadSenha"]."')";
-        pg_exec($con, $cadastro);
+    $cadEmail = $_POST["cadEmail"];
+    $cadNome = $_POST["cadNome"];
+    $cadSenha = $_POST["cadSenha"];
+    if (cadastrarUsuario($cadEmail, $cadNome, $cadSenha)) {
         echo "<script>cadastro.innerHTML='Usuário cadastrado com sucesso!';</script>"
         . " <style>#cadastro{color:green;}</style>";
+    } else {
+        echo "<script> cadastro.innerHTML='Este email já está sendo"
+        . " utilizado!';</script> <style>#cadastro{color:red;}</style>";
     }
 }
-
-echo " <script>
-      function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 14
-        });
-        var infoWindow = new google.maps.InfoWindow({map: map});
-
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
-
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('Você está aqui.');
-            map.setCenter(pos);
-          }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-          });
-        } else {
-         handleLocationError(false, infoWindow, map.getCenter());
-        }
-      }
-
-      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-                              'Error: The Geolocation service failed.' :
-                              'Error: Your browser doesn\'t support geolocation.');
-      }
-    </script>
-    <script src='https://maps.googleapis.com/maps/api/js?key=AIzaSyCHoafMtF7Sv5XiUhhTpnqv82PaGuFM3u4&callback=initMap'
-    async defer></script>";
+    initMap();
 ?>  
